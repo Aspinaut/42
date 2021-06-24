744c5adbff2c56d089334c0a6959ae82411764ae
Vincent Masse <vincentmasse@protonmail.com>
2021-06-24 15:59:16 +0200

    correcting strtrim

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,47 +6,48 @@
 /*   By: vmasse <vmasse@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/10 08:23:18 by vmasse            #+#    #+#             */
-/*   Updated: 2020/11/19 16:41:13 by vmasse           ###   ########.fr       */
+/*   Updated: 2021/06/24 15:55:17 by vmasse           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int  is_inset(char *set, char c)
+static int	is_inset(char *set, char c)
 {
-  int i;
+	int	i;
 
-  i = 0;
-  while (set[i])
-  {
-    if (set[i] == c)
-      return (1);
-    i++;
-  }
-  return (0);
+	i = 0;
+	while (set[i])
+	{
+		if (set[i] == c)
+			return (1);
+		i++;
+	}
+	return (0);
 }
 
-char  *ft_strtrim(char const *s1, char const *set)
+char	*ft_strtrim(char const *s1, char const *set)
 {
-  char    *s2;
-  int     i;
-  int     j;
-  int     k;
+	char	*s2;
+	int		i;
+	int		j;
+	int		k;
 
-  i = 0;
-  k = 0;
-  if (!s1 || !set)
-    return (NULL);
-  while (is_inset((char *)set, s1[i]))
-    i++;
-  j = ft_strlen((char *)s1) - 1;
-  while (is_inset((char *)set, s1[j]))
-    j--;
-  if (!(s2 = ft_strnew((j - i) + 1)))
-    return (NULL);
-  if (ft_strlen((char *)s1) == i)
-    return (ft_strdup(""));
-  while (i <= j)
-    s2[k++] = s1[i++];
-  return (s2);
+	i = 0;
+	k = 0;
+	if (!s1 || !set)
+		return (NULL);
+	while (is_inset((char *)set, s1[i]))
+		i++;
+	j = ft_strlen((char *)s1) - 1;
+	while (j > i && is_inset((char *)set, s1[j]))
+		j--;
+	s2 = ft_strnew((j - i) + 1);
+	if (!(s2))
+		return (NULL);
+	if (ft_strlen((char *)s1) == i)
+		return (ft_strdup(""));
+	while (i <= j)
+		s2[k++] = s1[i++];
+	return (s2);
 }
