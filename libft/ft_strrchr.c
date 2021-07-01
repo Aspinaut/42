09796f485ad9315b7b128246b836ed16b714454d
Vincent Masse <vincentmasse@protonmail.com>
2021-07-01 10:41:48 +0200

    correcting strrchr

--- libft/ft_strrchr.c
+++ libft/ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: vmasse <vmasse@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/08 08:48:33 by vmasse            #+#    #+#             */
-/*   Updated: 2021/06/30 20:18:47 by vmasse           ###   ########.fr       */
+/*   Updated: 2021/07/01 10:39:46 by vmasse           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,18 @@
 
 char	*ft_strrchr(const char *s, int c)
 {
-	int	len;
+	int	len_s;
 
-	len = ft_strlen((char *)s);
-	if (c == '\0')
-		return (ft_strchr(s, '\0'));
-	while (len >= 0)
+	len_s = ft_strlen((char *)s);
+	if (c == '\0' && c == s[len_s])
 	{
-		if (*(s + len) == (char)c)
-			return ((char *)s + len);
-		len--;
+		return ((char *)s + len_s);
+	}
+	while (len_s >= 0)
+	{
+		if (*(s + len_s) == (char)c)
+			return ((char *)s + len_s);
+		len_s--;
 	}
 	return (NULL);
 }
