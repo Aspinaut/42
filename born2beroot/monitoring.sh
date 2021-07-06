591c7088ec83502ebaa65456297d065beb4bc275
Vincent Masse <vincentmasse@protonmail.com>
2021-07-06 12:09:08 +0200

    born2beroot : almost done script

--- born2beroot/monitoring.sh
+++ born2beroot/monitoring.sh
@@ -8,18 +8,38 @@ disk_used=
 disk_total=
 percentage_disk_used=
 
+reboot_date=$(last reboot | head -1 | awk '{print $5,$6,$7,$8}')
+
+function check_lvm_usage {
+  isInFile=$(cat /etc/fstab | grep '^/dev/mapper/.*live-root')
+
+  if [ $isInFile ]
+  then
+    echo yes
+  else
+    echo no
+  fi
+}
+
 echo "#Architecture : $(uname -a)"
 echo "#CPU physical : $(grep "physical id" /proc/cpuinfo | sort | uniq | wc -l)"
 echo "#vCPU : $(grep "^processor" /proc/cpuinfo | wc -l)"
 
 # !! mem_used affiche aussi les unités
-
 echo "#Memory Usage : $(free -h --giga | grep Mem: | awk '{print $3}')/$(free -h --giga | grep Mem: | awk '{print $2}') ($(echo $percentage_mem_used | awk '{printf("%.2f",$1)}')%)"
-echo "#Disk Usage : $(uname -a)"
-echo "#CPU load : "$(uname -a)
-echo "#Last boot : "$(uname -a)
-echo "#LVM use : "$(uname -a)
-echo "#Connexions TCP : "$(uname -a)
-echo "#User log : "$(uname -a)
-echo "#Network : $(hostname -I)"
-echo "#Sudo : "$(uname -a
+
+# sur quelle partition ??
+echo "#Disk Usage : $()"
+
+# !! faut-il faire l'addition des cpus ou juste le 1er affiché ??
+echo "#CPU load : $(top -bn1 | grep '%Cpu(s):' | awk '{print $2}')%"
+echo "#Last boot : $(date --date=$reboot_date +%F\ %H:%M)"
+echo "#LVM use : $(check_lvm_usage)"
+echo "#Connexions TCP : $(netstat | grep '^tcp.*ESTABLISHED' | wc -l) ESTABLISHED"
+
+# nb d'users différents ou juste le nb de sessions ouvertes ? ex : 5 * vmasse42 pcq 5 terminaux ouverts
+echo "#User log : $(who | wc -l)"
+
+# !! bien vérifier l'adresse MAC (pq 2 adresses hors VM ??)
+echo "#Network : IP $(hostname -I)($(ip link show | grep link/ether | awk '{print $2}'))"
+echo "#Sudo : $(history | grep sudo | wc -l) cmd"
