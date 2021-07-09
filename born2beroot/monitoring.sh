4c575a0f17c49ff1bcd0c8361c770ec35b4ad9fd
Vincent Masse <vincentmasse@protonmail.com>
2021-07-09 13:40:21 +0200

    born2beroot : adding correct syntax for monitoring

--- born2beroot/monitoring.sh
+++ born2beroot/monitoring.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # code à insérer dans crontab -e en étant root :
-# /*10 * * * * /scripts/monitoring.sh
+# attention de bien chmod 755 monitoring.sh avant et de le placer dans /root
+# */10 * * * * ./monitoring.sh
 
 mem_used=$(free -t | grep Mem: | awk '{print $3}').0
 mem_total=$(free -t | grep Mem: | awk '{print $2}').0
@@ -23,28 +24,29 @@ function check_lvm_usage {
   fi
 }
 
-# ! trouver la bonne syntaxe pour faire fonctionner le wall
-wall $(
-echo "#Architecture : $(uname -a)\n"
-echo "#CPU physical : $(grep "physical id" /proc/cpuinfo | sort | uniq | wc -l)\n"
-echo "#vCPU : $(grep "^processor" /proc/cpuinfo | wc -l)\n"
+# ! trouver la bonne syntaxe pour faire fonctionner le wall, echo -e permet d'afficher le \n
 
-# !! mem_used affiche aussi les unités
-echo "#Memory Usage : $(free -h --giga | grep Mem: | awk '{print $3}')/$(free -h --giga | grep Mem: | awk '{print $2}') ($(echo $percentage_mem_used | awk '{printf("%.2f",$1)}')%)"
+( 
+  echo -e "#Architecture : $(uname -a)\n"
+  echo -e "#CPU physical : $(grep "physical id" /proc/cpuinfo | sort | uniq | wc -l)\n"
+  echo -e "#vCPU : $(grep "^processor" /proc/cpuinfo | wc -l)\n"
 
-# sur quelle partition ??
-echo "#Disk Usage :"
+  # !! mem_used affiche aussi les unités
+  echo -e "#Memory Usage : $(free -h --giga | grep Mem: | awk '{print $3}')/$(free -h --giga | grep Mem: | awk '{print $2}') ($(echo $percentage_mem_used | awk '{printf("%.2f",$1)}')%)\n"
 
-# !! faut-il faire l'addition des cpus ou juste le 1er affiché ??
-echo "#CPU load : $(top -bn1 | grep '%Cpu(s):' | awk '{print $2}')%"
-echo "#Last boot : $(date --date=$reboot_date +%F\ %H:%M)"
-echo "#LVM use : $(check_lvm_usage)"
-echo "#Connexions TCP : $(netstat | grep '^tcp.*ESTABLISHED' | wc -l) ESTABLISHED"
+  # sur quelle partition ??
+  echo -e "#Disk Usage :\n"
 
-# nb d'users différents ou juste le nb de sessions ouvertes ? ex : 5 * vmasse42 pcq 5 terminaux ouverts
-echo "#User log : $(who | wc -l)"
+  # !! faut-il faire l'addition des cpus ou juste le 1er affiché ??
+  echo -e "#CPU load : $(top -bn1 | grep '%Cpu(s):' | awk '{print $2}')%\n"
+  echo -e "#Last boot : $(date --date=$reboot_date +%F\ %H:%M)\n"
+  echo -e "#LVM use : $(check_lvm_usage)\n"
+  echo -e "#Connexions TCP : $(netstat | grep '^tcp.*ESTABLISHED' | wc -l) ESTABLISHED\n"
 
-# !! bien vérifier l'adresse MAC (pq 2 adresses hors VM ??)
-echo "#Network : IP $(hostname -I)($(ip link show | grep link/ether | awk '{print $2}'))"
-echo "#Sudo : $(history | grep sudo | wc -l) cmd"
-)
+  # nb d'users différents ou juste le nb de sessions ouvertes ? ex : 5 * vmasse42 pcq 5 terminaux ouverts
+  echo -e "#User log : $(who | wc -l)\n"
+
+  # !! bien vérifier l'adresse MAC (pq 2 adresses hors VM ??)
+  echo -e "#Network : IP $(hostname -I)($(ip link show | grep link/ether | awk '{print $2}'))\n"
+  echo -e "#Sudo : $(history | grep sudo | wc -l) cmd\n"
+) | wall
