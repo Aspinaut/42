3d789e2ba5fdd331002161574817e05e3f327920
Vincent Masse <vincentmasse@protonmail.com>
2021-07-12 20:05:42 +0200

    libft : correcting lstmap

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -6,19 +6,30 @@
 /*   By: vmasse <vmasse@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/04 17:17:52 by vmasse            #+#    #+#             */
-/*   Updated: 2021/07/04 17:27:12 by vmasse           ###   ########.fr       */
+/*   Updated: 2021/07/12 19:37:39 by vmasse           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void*))
 {
+	t_list	*new_elem;
+	t_list	*new_list;
+
 	if (!lst || !f || !del)
 		return (NULL);
-	while (lst->next)
+	new_list = NULL;
+	while (lst)
 	{
-		(*f)(lst->content);
+		new_elem = ft_lstnew(f(lst->content));
+		if (!new_elem)
+		{
+			ft_lstclear(&new_list, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&new_list, new_elem);
 		lst = lst->next;
 	}
+	return (new_list);
 }
