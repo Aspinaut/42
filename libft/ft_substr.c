b8d103f58ce57ee89eb8cb257801693fd37e13e7
Vincent Masse <vincentmasse@protonmail.com>
2021-07-15 13:34:54 +0200

    libft : protect s

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,27 +6,11 @@
 /*   By: vmasse <vmasse@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/14 17:03:49 by vmasse            #+#    #+#             */
-/*   Updated: 2021/07/15 13:11:25 by vmasse           ###   ########.fr       */
+/*   Updated: 2021/07/15 13:31:24 by vmasse           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-static void	*ft_memalloc_substr(size_t size)
-{
-	void	*allocated_area;
-
-	allocated_area = malloc(size);
-	if (!allocated_area)
-		return (NULL);
-	ft_bzero(allocated_area, size);
-	return (allocated_area);
-}
-
-static char	*ft_strnew_substr(size_t size)
-{
-	return ((char *)ft_memalloc_substr((size + 1) * (sizeof(char))));
-}
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
@@ -34,17 +18,17 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	size_t	i;
 	int		len_s;
 
-	i = 0;
-	len_s = ft_strlen(s);
 	if (!s)
 		return (NULL);
+	len_s = ft_strlen(s);
 	if (len_s < (int)start)
 		return (ft_strdup(""));
 	else if (len_s < ((int)start + (int)len))
 		len = len_s - start;
-	sample = ft_strnew_substr(len);
+	sample = (char *)malloc((len + 1) * sizeof(char));
 	if (!(sample))
 		return (NULL);
+	i = 0;
 	while (i < len)
 	{
 		sample[i] = s[start];
