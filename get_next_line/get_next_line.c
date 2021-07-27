a3719647198b6f427b3e3d5c1c62c5d791b9adf7
Vincent Masse <vincentmasse@protonmail.com>
2021-07-27 17:25:05 +0200

    gnl : working on else condition

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: vmasse <vmasse@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/26 15:15:44 by vmasse            #+#    #+#             */
-/*   Updated: 2021/07/27 10:51:53 by vmasse           ###   ########.fr       */
+/*   Updated: 2021/07/27 17:24:11 by vmasse           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,34 +15,28 @@
 char *get_next_line(int fd)
 {
     char buffer[BUFFER_SIZE];
-    static char **before_new_line;
+    static char *before_new_line;
+    char *temp;
 
-    *before_new_line = (char **)malloc(sizeof(char) * (4096 + 1));
+    before_new_line = NULL;
+    temp = NULL;
     while(read(fd, buffer, BUFFER_SIZE))
-    {
+    {   
+        printf("RCHR : %s\n", ft_strrchr(buffer, '\n'));
         if(ft_strrchr(buffer, '\n'))
         {
-            // printf("BUFFER IF char: %c\n", *buffer);
-            // printf("BUFFER IF str: %s\n", buffer);
-            
-            // si on trouve des chars apres le \n dans buffer, on les met dans static
-            
-            // ensuite on print buffer - tout ce qui se trouve apres le \n
-            printf("%s\n", ft_substr(buffer, 0, 3));
-            // write(1, ft_substr(buffer, 0, ), 1);
-            // et on avance au prochain tableau dans la static
-            before_new_line++;
+            before_new_line = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+            if (!before_new_line)
+                return (NULL);
+            before_new_line = ft_substr(buffer, 1, BUFFER_SIZE - 1);
+            printf("SUB : %s\n", ft_substr(buffer, 0, 3));
         }
         else
         {
-            // printf("BUFFER ELSE char: %c\n", *buffer);
-            // printf("BUFFER ELSE str: %s\n", buffer);
-            
-            // on met buffer dans la static
-            *before_new_line = buffer;
-            printf("STATIC : %s\n", before_new_line);
+            temp = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+            temp = ft_strjoin(before_new_line, buffer);
+            printf("ELSE : %s\n", temp);
         }
-        
     }
     return (before_new_line);
 }
