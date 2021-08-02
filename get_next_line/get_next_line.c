1d328ca8d6e34912a3f16ed47965e8cc89afc59f
Vincent Masse <vincentmasse@protonmail.com>
2021-08-02 19:00:47 +0200

    gnl : fix buffer segfault

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,13 +6,13 @@
 /*   By: vmasse <vmasse@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/26 15:15:44 by vmasse            #+#    #+#             */
-/*   Updated: 2021/08/02 16:44:08 by vmasse           ###   ########.fr       */
+/*   Updated: 2021/08/02 18:53:06 by vmasse           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int	ft_strchck_uniqueness(const char *s, char c)
+static int	ft_strchck_uniqueness(const char *s, char c)
 {
 	int total_char;
 
@@ -23,7 +23,7 @@ int	ft_strchck_uniqueness(const char *s, char c)
 	return (total_char);
 }
 
-static int check_static_end_line(char *s)
+static int  check_static_end_line(char *s)
 {
   int has_end_line;
 
@@ -39,7 +39,7 @@ static int check_static_end_line(char *s)
 
 char *get_next_line(int fd)
 {
-  char buffer[BUFFER_SIZE];
+  char buffer[BUFFER_SIZE + 1]; 
   static char *s;
   char *line;
 
@@ -48,6 +48,7 @@ char *get_next_line(int fd)
     s = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
     if (!s)
       return (NULL);
+    s[BUFFER_SIZE] = '\0'; // + 1 ?
   }
   line = NULL;
   if (check_static_end_line(s))
@@ -60,7 +61,7 @@ char *get_next_line(int fd)
   {
     if(ft_strrchr(buffer, '\n') && ft_strchck_uniqueness(buffer, '\n') == 1)
     {
-      if (buffer[ft_strlen(buffer) - 1] == '\n')
+      if (buffer[BUFFER_SIZE] == '\n')
       {
         line = ft_strjoin(s, buffer);
         free(s);
@@ -68,38 +69,40 @@ char *get_next_line(int fd)
       }
       else
       {
-        line = ft_strjoin(s, ft_substr(buffer, 0, ft_strchrpos(buffer, '\n') + 1));
+        line = ft_strjoin(s, ft_substr(buffer, 0, ft_strchrpos(buffer, '\n'))); // + 1 ?
         free(s);
-        s = ft_substr(buffer, ft_strchrpos(buffer, '\n') + 1, BUFFER_SIZE - 1);
+        s = ft_substr(buffer, ft_strchrpos(buffer, '\n') + 1, BUFFER_SIZE);
         return (line);
       }
     }
-    else if (ft_strrchr(buffer, '\n') && ft_strchck_uniqueness(buffer, '\n') > 1)
+    else //if (ft_strrchr(buffer, '\n') && ft_strchck_uniqueness(buffer, '\n') > 1)
     {
       s = ft_strjoin(s, buffer);
       return (get_next_line(fd));
     }
-    else
-    {
-      s = ft_strjoin(s, buffer);
-      return (get_next_line(fd));
-    }
+    // else
+    // {
+    //   s = ft_strjoin(s, buffer);
+    //   return (get_next_line(fd));
+    // }
   }
   return (s);
 }
 
-int main()
-{
-    int fd;
-    char *line;
-    int i;
+// int main(int ac, char **ag)
+// {
+//     char *s;
+//     int fd;
 
-    fd = open("file.txt", O_RDONLY);
-    i = 0;
-    while (i < 7)
-    {
-      printf("GNL (%d) : %s\n", i, get_next_line(fd));
-      i++;
-    }
-    return (0);
-}
+//     fd = open(ag[1], O_RDONLY);
+//     s = get_next_line(fd);
+//     while (s)
+//     {
+//       printf("|%s", s);
+//       free(s);
+//       s = get_next_line(fd);
+//     }
+//     close(fd);
+//     system("leaks gnl");
+//     return (0);
+// }
