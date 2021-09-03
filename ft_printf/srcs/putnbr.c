1d6aa04c76dc006996ca0e3f4bd843b852bdbf2d
Vincent Masse <vincentmasse@protonmail.com>
2021-09-03 22:58:09 +0200

    add basic putnbr hex

--- ft_printf/srcs/putnbr.c
+++ ft_printf/srcs/putnbr.c
@@ -6,7 +6,7 @@
 /*   By: vmasse <vmasse@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/21 19:08:34 by vmasse            #+#    #+#             */
-/*   Updated: 2021/08/21 19:16:15 by vmasse           ###   ########.fr       */
+/*   Updated: 2021/09/03 16:18:29 by vmasse           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,4 +43,32 @@ void	ft_putnbr(t_var *vartab, int n)
 		ft_putnbr(vartab, nbr / 10);
 		vartab->len_to_print += ft_putchar(nbr % 10 + '0');
 	}
+}
+
+static char convert_int_to_hex(int nb)
+{
+	char
+}
+
+void	ft_putnbr_hex(int nb)
+{
+	int quotient;
+	int remainder;
+	int nb_cpy;
+
+	nb_cpy = nb;
+	quotient = 0;
+	remainder = 0;
+	while (nb > 16)
+	{
+		quotient = nb / 16;
+		remainder = nb - (quotient * 16);
+		ft_putchar(nb + '0');
+	}
+}
+
+int main()
+{
+	// ft_putnbr_hex();
+	printf("%x\n", 689321);
 }
