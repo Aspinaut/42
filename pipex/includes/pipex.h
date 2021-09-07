06b969192b2f26cdd9b094902ef195d078414c39
Vincent Masse <vincentmasse@protonmail.com>
2021-09-07 14:06:42 +0200

    pipex : init project

---
+++ pipex/includes/pipex.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vmasse <vmasse@student.s19.be>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/07 13:18:58 by vmasse            #+#    #+#             */
+/*   Updated: 2021/09/07 13:41:50 by vmasse           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <unistd.h> 
+# include <sys/wait.h> 
+
+// check la bonne pour error - strerror
+# include <stdio.h> 
+# include <string.h> 
+
+
+
+#endif
---
+++ pipex/srcs/pipex.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vmasse <vmasse@student.s19.be>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/07 13:06:25 by vmasse            #+#    #+#             */
+/*   Updated: 2021/09/07 14:04:12 by vmasse           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/pipex.h"
+
+int main(int argc, char **argv)
+{
+    // READ from infile
+    // EXECUTE cmd1 with infile as input
+    // SEND output to cmd2
+    // WRITE to outfile
+     
+    return (0);
+}
