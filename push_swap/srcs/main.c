c184a8399589b07103133297f1addd3b7088e1b8
Vincent Masse <vincentmasse@protonmail.com>
2021-10-13 16:26:54 +0200

    push_swap : parsing still WIP

--- push_swap/srcs/main.c
+++ push_swap/srcs/main.c
@@ -6,29 +6,36 @@
 /*   By: vmasse <vmasse@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/19 18:21:16 by vmasse            #+#    #+#             */
-/*   Updated: 2021/10/08 15:20:40 by vmasse           ###   ########.fr       */
+/*   Updated: 2021/10/13 16:26:25 by vmasse           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
-int	main(int argc, char **argv)
+int	find_spaces(char **argv)
 {
-	t_stack *stack_a;
-	t_stack *stack_b;
 	int i;
+	int j;
 
-	if (argc == 1)
-		return (0);
-	argv = ft_split(argv[1], '"');
 	i = 0;
 	while (argv[i])
 	{
-		printf("%s\n", argv[i]);
+		j = 0;
+		while (argv[i][j])
+		{
+			if (argv[i][j] == ' ')
+				return (1);
+			j++;
+		}
 		i++;
 	}
-	if (!validate_args(argc, argv))
-		return (ft_putstr_fd("Error\n", 2));
+	return (0);
+}
+
+void	fill_stack(t_stack **stack_a, int argc, char **argv)
+{
+	int i;
+
 	stack_a = stack_addnew(ft_atoi(argv[1]));
 	stack_b = NULL;
 	if (!stack_a)
@@ -36,6 +43,19 @@ int	main(int argc, char **argv)
 	i = 1;
 	while (++i <= (argc - 1))
 		stack_addback(&stack_a->next, stack_addnew(ft_atoi(argv[i])));
+}
+
+int	main(int argc, char **argv)
+{
+	t_stack *stack_a;
+	t_stack *stack_b;
+
+	if (argc == 1)
+		return (0);
+	if (!validate_args(argc, argv))
+		return (ft_putstr_fd("Error\n", 2));
+	
+	fill_stack(&stack_a, argc , argv);
 
 	// small_sort(&stack_a, &stack_b, stack_size(stack_a));
 	// big_sort(&stack_a, &stack_b);
