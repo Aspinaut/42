6ee811760449346810867931eae628cd1782d290
Vincent Masse <vincentmasse@protonmail.com>
2021-10-25 12:10:00 +0200

    so_long : successfully plugged mlx

--- so_long/includes/so_long.h
+++ so_long/includes/so_long.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   so_long.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vmasse <vmasse@student.42.fr>              +#+  +:+       +#+        */
+/*   By: vmasse <vmasse@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/15 11:24:37 by vmasse            #+#    #+#             */
-/*   Updated: 2021/10/22 17:46:11 by vmasse           ###   ########.fr       */
+/*   Updated: 2021/10/25 12:07:13 by vmasse           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,9 @@
 # define SO_LONG_H
 
 #include "../mlx/mlx.h"
-#include "../mlx/mlx_new_window.h"
-
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 
 
--- so_long/srcs/main.c
+++ so_long/srcs/main.c
@@ -3,21 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vmasse <vmasse@student.42.fr>              +#+  +:+       +#+        */
+/*   By: vmasse <vmasse@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/22 17:14:31 by vmasse            #+#    #+#             */
-/*   Updated: 2021/10/22 17:44:19 by vmasse           ###   ########.fr       */
+/*   Updated: 2021/10/25 12:05:13 by vmasse           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/so_long.h"
 
-int main()
+typedef struct    data_s
 {
-    void *mlx;
+    void          *mlx_ptr;
+    void          *mlx_win;
+}                 data_t;
 
-    mlx = 0;
-    mlx_init();
-    mlx_new_window(mlx, 1600, 1200, "So long");
-    return (0);
+int main(void)
+{
+    data_t        data;
+
+    if ((data.mlx_ptr = mlx_init()) == NULL)
+        return (EXIT_FAILURE);
+    if ((data.mlx_win = mlx_new_window(data.mlx_ptr, 640, 480, "Hello world")) == NULL)
+        return (EXIT_FAILURE);
+    mlx_loop(data.mlx_ptr);
+    return (EXIT_SUCCESS);
 }
