6c8d2eb43b6db2527ab349beb036fa8b1343ffb8
Vincent Masse <vincentmasse@protonmail.com>
2021-10-27 12:43:17 +0200

    so_long : add basic movements

--- so_long/includes/so_long.h
+++ so_long/includes/so_long.h
@@ -6,18 +6,58 @@
 /*   By: vmasse <vmasse@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/15 11:24:37 by vmasse            #+#    #+#             */
-/*   Updated: 2021/10/25 12:07:13 by vmasse           ###   ########.fr       */
+/*   Updated: 2021/10/27 12:40:19 by vmasse           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
-#include "../mlx/mlx.h"
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
+# include "../mlx/mlx.h"
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
 
+# define WIN_WIDTH 	1600
+# define WIN_HEIGHT 1200
+
+# define UP 		65362
+# define DOWN 		65364
+# define LEFT 		65361
+# define RIGHT 		65363
+# define ESCAPE 	65307
+
+typedef struct    s_img
+{
+	void 			*img_ptr;
+	char 			*addr;
+	char			*path;
+	int 			x;
+	int				y;
+	int 			width;
+	int				height;
+	int 			*bits_per_pixel;
+	int 			*size_line;
+	int 			*endian;
+	struct s_img 	*next;
+}                 t_img;
+
+typedef struct    s_player
+{
+	struct s_img	*img;
+}                 t_player;
+
+typedef struct    s_game
+{
+    void			*mlx_ptr;
+    void			*win_ptr;
+	struct s_img	*img;
+	struct s_player	*player;
+}                 t_game;
+
+
+int process_key_hook(int keycode, t_game *game);
+void move_player(int kc, t_game *game);
 
 
 #endif
--- so_long/srcs/main.c
+++ so_long/srcs/main.c
@@ -6,26 +6,72 @@
 /*   By: vmasse <vmasse@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/22 17:14:31 by vmasse            #+#    #+#             */
-/*   Updated: 2021/10/25 12:05:13 by vmasse           ###   ########.fr       */
+/*   Updated: 2021/10/27 12:41:08 by vmasse           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/so_long.h"
 
-typedef struct    data_s
+void move_player(int kc, t_game *game)
 {
-    void          *mlx_ptr;
-    void          *mlx_win;
-}                 data_t;
+	mlx_clear_window(game->mlx_ptr, game->win_ptr);
+	if (kc == UP)
+		mlx_put_image_to_window(game->mlx_ptr, game->win_ptr, game->img->img_ptr, game->img->x, game->img->y--);
+	else if (kc == DOWN)
+		mlx_put_image_to_window(game->mlx_ptr, game->win_ptr, game->img->img_ptr, game->img->x, game->img->y++);
+	else if (kc == LEFT)
+		mlx_put_image_to_window(game->mlx_ptr, game->win_ptr, game->img->img_ptr, game->img->x--, game->img->y);
+	else if (kc == RIGHT)
+		mlx_put_image_to_window(game->mlx_ptr, game->win_ptr, game->img->img_ptr, game->img->x++, game->img->y);
+}
+
+int process_key_hook(int kc, t_game *game)
+{
+	// printf("%d\n", kc);
+	if (kc == ESCAPE)
+		return (mlx_loop_end(game->mlx_ptr));
+	else if (kc == UP || kc == DOWN || kc == LEFT || kc == RIGHT)
+		move_player(kc, game);
+	return (1);
+}
 
 int main(void)
 {
-    data_t        data;
+    t_game        game;
+	t_img			img;
 
-    if ((data.mlx_ptr = mlx_init()) == NULL)
-        return (EXIT_FAILURE);
-    if ((data.mlx_win = mlx_new_window(data.mlx_ptr, 640, 480, "Hello world")) == NULL)
-        return (EXIT_FAILURE);
-    mlx_loop(data.mlx_ptr);
+	game.mlx_ptr = mlx_init();
+    if (game.mlx_ptr == NULL)
+		return (EXIT_FAILURE);
+	game.win_ptr = mlx_new_window(game.mlx_ptr, WIN_WIDTH, WIN_HEIGHT, "Hello world");
+    if (!game.win_ptr)
+		return (EXIT_FAILURE);
+
+	// img.img_ptr = mlx_new_image(game.mlx_ptr, 40, 40);
+	// img.addr = mlx_get_data_addr(img.img_ptr, &img.bits_per_pixel, &img.size_line, &img.endian);
+
+	img.path = "/home/vmasse/Code/42/so_long/images/ralph.xpm";
+	img.width = 50;
+	img.height = 35;
+	img.y = 0;
+	img.x = 0;
+	img.img_ptr = mlx_xpm_file_to_image(game.mlx_ptr, img.path, &img.width, &img.height);
+	mlx_put_image_to_window(game.mlx_ptr, game.win_ptr, img.img_ptr, img.x, img.y);
+
+	game.img = &img;
+	// while (img.x < WIN_WIDTH)
+	// {
+	// 	mlx_clear_window(game.mlx_ptr, game.win_ptr);
+	// 	mlx_put_image_to_window(game.mlx_ptr, game.win_ptr, img.img_ptr, img.x, img.y);
+	// 	if (img.x + 1 == WIN_WIDTH)
+	// 		img.x = 0;
+	// 	img.x++;
+	// }
+
+	mlx_key_hook(game.win_ptr, process_key_hook, &game);
+	// int	mlx_expose_hook (void *win_ptr, int (*funct_ptr)(), void *param);
+	// int	mlx_loop_hook (void *mlx_ptr, int (*funct_ptr)(), void *param);
+
+	mlx_loop(game.mlx_ptr);
     return (EXIT_SUCCESS);
 }
