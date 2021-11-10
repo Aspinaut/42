e6459f7f1677b65b700a745f65ee7377c8b30bbc
Vincent Masse <vincentmasse@protonmail.com>
2021-11-10 20:36:54 +0100

    exams : fix putnbr

--- exams/exam02/ft_printf.c
+++ exams/exam02/ft_printf.c
@@ -6,6 +6,15 @@ typedef struct s_arg
 	int len;
 }	t_arg;
 
+int ft_strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+	return (i);
+}
+
 int ft_putchar(char c)
 {
 	return (write(1, &c, 1));
@@ -14,7 +23,6 @@ int ft_putchar(char c)
 int ft_putstr(char *s)
 {
 	int i = 0;
-
 	while (s[i])
 	{
 		write(1, &s[i], 1);
@@ -23,88 +31,93 @@ int ft_putstr(char *s)
 	return (i);
 }
 
-void ft_putnbr(int nb, t_arg *arg)
+void	ft_putnbr(int n, t_arg *arg)
 {
-	if (nb < 0)
+	long	nbr;
+
+	nbr = n;
+	if (nbr < 0)
 	{
-		nb = nb * -1;
+		nbr = nbr * -1;
 		arg->len += ft_putchar('-');
 	}
-	if (nb > 9)
+	if (nbr >= 0 && nbr < 10)
 	{
-		ft_putnbr(nb / 10, arg);
-		arg->len += ft_putchar(nb % 10 + '0');
+		arg->len += ft_putchar(nbr + '0');
 	}
 	else
 	{
-		arg->len += ft_putchar(nb + '0');
+		ft_putnbr(nbr / 10, arg);
+		arg->len += ft_putchar(nbr % 10 + '0');
 	}
 }
 
-void ft_putnbr_hex(t_arg *arg, unsigned int nb, unsigned int converter)
+void ft_putnbr_hex(unsigned int nb, t_arg *arg, unsigned int converter)
 {
 	unsigned int remainder = 0;
-	unsigned int nb_cpy;
+	unsigned int nb_cpy = nb;
 
-	nb_cpy = nb;
 	if (nb > 15)
 	{
 		nb = nb / 16;
 		remainder = nb_cpy - (nb * 16);
 		if (remainder < 10)
 		{
-			ft_putnbr_hex(arg, nb, converter);
-			arg->len += ft_putchar(remainder + '0');
+			ft_putnbr_hex(nb, arg, converter);
+			arg->len += ft_putchar(remainder + '0'); 
 		}
 		else
 		{
-			ft_putnbr_hex(arg, nb, converter);
-			arg->len += ft_putchar(remainder + (converter - 23) - 10);
+			ft_putnbr_hex(nb, arg, converter);
+			arg->len += ft_putchar(remainder + converter - 33); 
 		}
 	}
 	else if (nb > 9 && nb < 16)
-		arg->len += ft_putchar(nb + (converter - 23) - 10);
-	else
-		arg->len += ft_putchar(nb + '0');
+	{
+		arg->len += ft_putchar(nb + converter - 33); 
+	}
+	else 
+	{
+		arg->len += ft_putchar(nb + '0'); 
+	}
 }
 
-int ft_printf(char const *format, ...)
+int ft_printf(const char *format, ...)
 {
 	t_arg arg;
-	int pos = -1;
+	int i = -1;
 	va_list args;
-	char *s = NULL;
-	int nb;
+	char *s;
 
 	va_start(args, format);
 	arg.len = 0;
-	while (format[++pos])
+	while (format[++i])
 	{
-		if (format[pos] == '%' && format[pos + 1] == 's')
+		if (format[i] == '%')
 		{
-			s = va_arg(args, char *);
-			if (!s)
-				arg.len += ft_putstr("(null)");
-			else
-				arg.len += ft_putstr(s);
-			pos++;
+			if (format[i + 1] == 's')		
+			{
+				s = va_arg(args, char *);
+				if (!s)
+					arg.len += ft_putstr("(null)");
+				else
+					arg.len += ft_putstr(s);
+			}
+			else if (format[i + 1] == 'd')		
+			{
+				ft_putnbr(va_arg(args, int), &arg);
+			}
+			else if (format[i + 1] == 'x')		
+			{
+				ft_putnbr_hex(va_arg(args, unsigned int), &arg, 'x');
+			}
+			i++;
 		}
-		else if (format[pos] == '%' && format[pos + 1] == 'd')
+		else		
 		{
-			ft_putnbr(va_arg(args, int), &arg);
-			pos++;
-		}
-		else if (format[pos] == '%' && format[pos + 1] == 'x')
-		{
-			nb = va_arg(args, unsigned);
-			ft_putnbr_hex(&arg, nb, 'x');
-			pos++;
-		}
-		else
-		{
-			arg.len += ft_putchar(format[pos]);
+			arg.len += write(1, &format[i], 1);
 		}
 	}
 	va_end(args);
-	return(arg.len);
+	return (arg.len);
 }
