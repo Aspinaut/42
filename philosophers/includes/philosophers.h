185ff73fe295b1236601224157b36b1e67f18559
Vincent Masse <vincentmasse@protonmail.com>
2021-12-08 17:23:06 +0100

    philo : init philos

--- philosophers/includes/philosophers.h
+++ philosophers/includes/philosophers.h
@@ -6,13 +6,19 @@
 # include <unistd.h>
 # include <stdlib.h>
 
+# define	MAX_LONG	9223372036854775807
+
 typedef struct s_phi
 {
-    int to_die;
-    int to_eat;
-    int to_sleep;
-    int eat_max;
-}               t_phi;
+    int             to_die;
+    int             to_eat;
+    int             to_sleep;
+    int             eat_max;
+    int             id;
+    pthread_mutex_t mutex;
+    struct s_phi    *next;
+    struct s_phi    *prev;
+}                   t_phi;
 
 /* PARSING */
 
@@ -22,6 +28,6 @@ int     check_args(char **argv);
 
 int     ft_strlen(const char *str);
 void    ft_exit(char *msg);
-
+int	ft_atoi(const char *str);
 
 #endif
--- philosophers/srcs/main.c
+++ philosophers/srcs/main.c
@@ -6,53 +6,85 @@
 /*   By: vmasse <vmasse@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/04 16:51:42 by vmasse            #+#    #+#             */
-/*   Updated: 2021/12/04 17:17:40 by vmasse           ###   ########.fr       */
+/*   Updated: 2021/12/08 17:22:09 by vmasse           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philosophers.h"
 
-void *p1()
+// void *p1()
+// {
+//   int i = 0;
+//   char s[1500] = "Bonjour !Bonjour !Bonjour !Bonjour !Bonjour !Bonjour !Bonjour !Bonjour !Bonjour !Bonjour !Bonjour !Bonjour !Bonjour !Bonjour !Bonjour !Bonjour !Bonjour !Bonjour !Bonjour !Bonjour !Bonjour !Bonjour !Bonjour !Bonjour !Bonjour !";
+
+//   while (s[i])
+//   {
+//     // usleep(100);
+//     fprintf(stderr, "%c\n", s[i]);
+//     i++;
+//   }
+//   return (NULL);
+// }
+
+// void *p2()
+// {
+//   int i = 0;
+//   char s[1500] = "Salut !Salut !Salut !Salut !Salut !Salut !Salut !Salut !Salut !Salut !Salut !Salut !Salut !Salut !Salut !Salut !Salut !Salut !Salut !Salut !Salut !Salut !Salut !Salut !Salut !Salut !Salut !Salut !Salut !Salut !Salut !Salut !";
+
+//   while (s[i])
+//   {
+//     // usleep(100);
+//     fprintf(stderr, "%c\n", s[i]);
+//     i++;
+//   }
+//   return (NULL);
+// }
+
+void init_philos(t_phi *philo, char **argv)
 {
-  int i = 0;
-  char s[1500] = "Bonjour !Bonjour !Bonjour !Bonjour !Bonjour !Bonjour !Bonjour !Bonjour !Bonjour !Bonjour !Bonjour !Bonjour !Bonjour !Bonjour !Bonjour !Bonjour !Bonjour !Bonjour !Bonjour !Bonjour !Bonjour !Bonjour !Bonjour !Bonjour !Bonjour !";
+  int     i;
+  t_phi   prev;
+  int     stop;
 
-  while (s[i])
+  stop = ft_atoi(argv[1]);
+  i = -1;
+  while (++i < stop)
   {
-    // usleep(100);
-    fprintf(stderr, "%c\n", s[i]);
-    i++;
+    philo->to_die = ft_atoi(argv[2]);
+    philo->to_eat = ft_atoi(argv[3]);
+    philo->to_sleep = ft_atoi(argv[4]);
+    philo->id = i;
+    if (argv[5])
+      philo->eat_max = ft_atoi(argv[5]);
+    if (i == 0)
+      philo->prev = NULL;
+    else 
+    {
+      philo->prev = &prev;
+      philo->prev->next = philo;
+    }
+    prev = *philo;
+    philo->next = NULL;
   }
-  return (NULL);
-}
-
-void *p2()
-{
-  int i = 0;
-  char s[1500] = "Salut !Salut !Salut !Salut !Salut !Salut !Salut !Salut !Salut !Salut !Salut !Salut !Salut !Salut !Salut !Salut !Salut !Salut !Salut !Salut !Salut !Salut !Salut !Salut !Salut !Salut !Salut !Salut !Salut !Salut !Salut !Salut !";
-
-  while (s[i])
-  {
-    // usleep(100);
-    fprintf(stderr, "%c\n", s[i]);
-    i++;
-  }
-  return (NULL);
 }
 
 int main(int argc, char **argv)
 {
-  pthread_t t1;
-  pthread_t t2;
-  // t_phi     philo;
+  t_phi     philo;
 
   if (argc != 5 && argc != 6)
     ft_exit("Error\nWrong nb of arguments\n");
   if (!check_args(argv))
     ft_exit("Error\nSomething wrong with arguments\n");
-  pthread_create(&t1, NULL, p1, NULL);
-  pthread_create(&t2, NULL, p2, NULL);
-  pthread_join(t2, NULL);
-  pthread_join(t1, NULL);
+  init_philos(&philo, argv);
+  while (philo)
+  {
+    printf("%d\n", philo);
+    philo = philo.next;
+  }
+  // pthread_create(&t1, NULL, p1, NULL);
+  // pthread_create(&t2, NULL, p2, NULL);
+  // pthread_join(t2, NULL);
+  // pthread_join(t1, NULL);
   return (0);
 }
