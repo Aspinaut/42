32b830a37515d1f36c6fdcd3ec066309179840d5
Aspinaut <vincentmasse@protonmail.com>
2022-03-15 16:38:00 +0100

    cpp00/ex00 : add protection for megaphone

--- cpp/cpp00/ex00/Megaphone.cpp
+++ cpp/cpp00/ex00/Megaphone.cpp
@@ -6,7 +6,10 @@ Megaphone::~Megaphone(void) {}
 
 void Megaphone::turnOn(std::string s) {
 	for (int i = 0; i < s.length(); i++) {
-		putchar(toupper(s[i]));
+		if (s[i] >= 'a' && s[i] <= 'z')
+			putchar(toupper(s[i]));
+		else
+			putchar(s[i]);
 	}
 	std::cout << std::endl;
 }
