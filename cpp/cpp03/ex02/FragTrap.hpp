36a640722cf463031c9d8367857cde3636b11a48
Aspinaut <vincentmasse@protonmail.com>
2022-03-28 11:33:00 +0200

    cpp03 : Makefile modifs

--- cpp/cpp03/ex02/FragTrap.hpp
+++ cpp/cpp03/ex02/FragTrap.hpp
@@ -1,25 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   FragTrap.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vmasse <vmasse@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/26 12:16:09 by vmasse            #+#    #+#             */
-/*   Updated: 2022/03/28 11:30:30 by vmasse           ###   ########.fr       */
+/*   Created: 2022/03/28 11:24:33 by vmasse            #+#    #+#             */
+/*   Updated: 2022/03/28 11:27:31 by vmasse           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "FragTrap.hpp"
+#ifndef FRAGTRAP_HPP
+# define FRAGTRAP_HPP
+# include "ScavTrap.hpp"
 
-int main()
+class FragTrap : public ClapTrap
 {
-	ClapTrap clappy("Clappy");
-	FragTrap fraggy("Fraggy");
-	
-	std::cout << fraggy.getName() <<  " has : " << fraggy.getEnergyPts() << " energy points." << std::endl;
-	fraggy.attack("Clappy");
-	clappy.takeDamage(fraggy.getAttackDmg());
-	fraggy.highFivesGuys();
-	return 0;
-}
+	public:
+
+		FragTrap();
+		FragTrap(std::string name);
+		~FragTrap();
+		void attack(const std::string& target);
+		void guardGate();
+		void highFivesGuys(void);
+};
+
+#endif
