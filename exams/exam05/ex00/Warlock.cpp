8962c1c01e8e2ceca3fbbbe199a001e46379dbbb
Vincent Masse <vincentmasse@protonmail.com>
2022-07-04 15:19:50 +0200

    exams : fix Warlock constructor

--- exams/exam05/ex00/Warlock.cpp
+++ exams/exam05/ex00/Warlock.cpp
@@ -1,11 +1,6 @@
 #include "Warlock.hpp"
 
-Warlock::Warlock(const string n, const string t) : _name(n), _title(t)
-{
-	cout << _name << ": This looks like another boring day." << endl;
-}
-
-Warlock::Warlock(const Warlock &rhs) : _name(rhs._name), _title(rhs._title)
+Warlock::Warlock(const string &n, const string &t) : _name(n), _title(t)
 {
 	cout << _name << ": This looks like another boring day." << endl;
 }
@@ -20,9 +15,9 @@ void Warlock::introduce() const
 	cout << _name << ": I am " << _name << ", " << _title << "!" << endl;
 }
 
-void Warlock::setTitle(const string t) { _title = t; }
-void Warlock::setName(const string n) { _name = n; }
+void Warlock::setTitle(const string &t) { _title = t; }
+void Warlock::setName(const string &n) { _name = n; }
 
 
-string Warlock::getTitle() const { return _title; }
-string Warlock::getName() const { return _name; }
+string const Warlock::getTitle() const { return _title; }
+string const Warlock::getName() const { return _name; }
--- exams/exam05/ex00/Warlock.hpp
+++ exams/exam05/ex00/Warlock.hpp
@@ -6,19 +6,21 @@ using namespace std;
 class Warlock
 {
 	public:
-		Warlock(const string name, const string title);
-		Warlock(const Warlock &rhs);
+		Warlock(const string &name, const string &title);
 		~Warlock();
 	
 		void introduce() const;
 
-		void setName(string name);
-		string getName() const;
-		void setTitle(string title);
-		string getTitle() const;
+		void setName(const string &name);
+		const string getName() const;
+		void setTitle(const string &title);
+		const string getTitle() const;
 
 	private:
 		string _name;
 		string _title;
 
+		Warlock();
+        Warlock(Warlock const &other);
+        Warlock &operator=(Warlock const &other);
 };
